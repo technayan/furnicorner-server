a2e11c051a1bde37d64be24528da7cd04e4f21fd
dev_nayan <md.nayan8494@gmail.com>
2022-05-23 01:59:01 +0600

    MongoDB Connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,11 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ycnj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 // API's
 app.get('/', (req, res) => {
