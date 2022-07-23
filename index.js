91b7e1869ef7b06b94bd1c54ed8dd9728e293cd2
dev_nayan <md.nayan8494@gmail.com>
2022-07-23 12:00:51 +0600

    Some Modification

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 // Connect to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ycnj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
+
 async function run() {
     try {
         await client.connect();
